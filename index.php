bf6ba18de0d4a92c44c7fe88a36a85abadb7c051
vinegod <denys.vinegod@gmail.com>
2022-01-02 12:51:06 +0200

    first files

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Vinegod</title>
+	<link rel="stylesheet" type="text/css" href="/Templates/style.css">
+</head>
+
+<body>
+	<div class="header">
+		<div class="whole_wrapper">
+			header
+		</div>
+	</div>
+	
+	<div class="content">
+		<div class="whole_wrapper">
+			content
+		</div>
+	</div>
+	
+	<div class="footer">
+		<div class="whole_wrapper">
+			footer
+		</div>
+	</div>
+</body>
+</html>
